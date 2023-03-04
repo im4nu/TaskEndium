22bb4fb4b99770411bc958b82be76610853574bd
eduardoaraujomilfont <eduardoaraujomilfont@gmail.com>
2023-03-04 01:04:56 -0300

    Botão do Eduardo button implementation

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,4 +1,4 @@
-import { Text, VStack } from 'native-base';
+import { View, Text, VStack, Button, Toast } from 'native-base';
 
 export function Home() {
   return (
@@ -7,9 +7,32 @@ export function Home() {
       bg={'green.200'}
       justifyContent={'center'}
       textAlign={'center'}
+      alignItems={'center'}
       px={8}
     >
       <Text>Bem vindo a Home</Text>
+
+      <View 
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+      }}>
+
+        <Button  
+        variant="subtle" 
+        justifyContent={'center'}
+        textAlign={'center'} 
+        key={50} 
+        size={130}
+        colorScheme="secondary" 
+        onPress={() => Toast.show({
+          description: "Pop-up do Eduardo"
+        })}> 
+        Botão do Eduardo
+        </Button>
+      </View>
+
     </VStack>
   );
 }
