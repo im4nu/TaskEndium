c4b87d3c3d1522dfa62c54033d88ccaaa00d63fd
Italo Gonçalves <italog.luciajno@gmail.com>
2023-04-02 15:17:22 -0300

    explore card

--- src/screens/home/components/CardExplore.tsx
+++ src/screens/home/components/CardExplore.tsx
@@ -10,7 +10,32 @@ interface ExploreProps {
 export function CardExplore({titulo, info, textbutton }: ExploreProps) {
   return (
     <View width={'100%'}>
-     
+      <HStack background={'#D2D2D2'} 
+      borderRadius={12} 
+      marginTop={4} 
+      alignItems={'center'} 
+      justifyContent={'center'}
+      >
+          <VStack padding={3} flex={3}>
+          <Text fontSize={30} color={'#222222'} >{titulo}</Text>
+          <Text width={'90%'} fontSize={14} paddingY={2} color={'#222222'}>{info}</Text>
+          <View
+            borderColor={'black'}
+            borderWidth={4}
+            borderRadius={4}
+            alignItems={'center'}
+            justifyContent={'center'}
+            width={'70%'}
+            marginTop= {2}
+          >
+            <Text fontSize={18} padding={1} color={'#222222'}>{textbutton}</Text>
+          </View>
+        </VStack>
+
+        <VStack alignContent={'center'} justifyContent={'center'} flex={2}>
+          <Image source={image} alt= {'icone button'} height={200} borderRightRadius={11}/>
+        </VStack>
+      </HStack>
     </View>
   );
 }
