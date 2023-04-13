fc5b908946a2efc42a8b4a462c0e164125a4a863
Italo Gonçalves <italog.luciajno@gmail.com>
2023-04-13 15:42:17 -0300

    page habitos estatica

--- src/screens/habitos/index.tsx
+++ src/screens/habitos/index.tsx
@@ -1,6 +1,7 @@
-import { Text, VStack, Button, View } from 'native-base';
+import { HStack, ScrollView, Text, View, VStack } from 'native-base';
 import { ImageBackground } from 'react-native';
 import { background } from '../../utils/images';
+import { TaskCard } from '../../components/TasksCards';
 
 export function Habitos() {
   return (
@@ -10,10 +11,53 @@ export function Habitos() {
       resizeMode={'cover'}
       style={{ flex: 1 }}
     >
-      <VStack flex={1} bg={'#FFC70035'}>
-        <VStack flex={1} justifyContent={'center'} textAlign={'center'} px={8}>
-          <Text>Bem vindo à tela Hábitos</Text>
+     <VStack flex={1} bg={'#FFC70035'}>
+        <ScrollView>
+        <VStack flex={1} justifyContent={'flex-start'} textAlign={'center'} px={8} paddingTop={10}>
+          <HStack>
+            <TaskCard Month='Fev' Day={17} Title='Dentista' BoxColor='#FFC700'></TaskCard>
+            <TaskCard Month='Fev' Day={17} Title='Dentista' BoxColor='#FFC700'></TaskCard>
+            <TaskCard Month='Fev' Day={17} Title='Dentista' BoxColor='#FFC700'></TaskCard>
+          </HStack>
+
+          <HStack>
+            <TaskCard Month='Fev' Day={17} Title='Dentista' BoxColor='#FFC700'></TaskCard>
+            <TaskCard Month='Fev' Day={17} Title='Dentista' BoxColor='#FFC700'></TaskCard>
+            <TaskCard Month='Fev' Day={17} Title='Dentista' BoxColor='#FFC700'></TaskCard>
+          </HStack>
+          <HStack>
+            <TaskCard Month='Fev' Day={17} Title='Dentista' BoxColor='#FFC700'></TaskCard>
+            <TaskCard Month='Fev' Day={17} Title='Dentista' BoxColor='#FFC700'></TaskCard>
+            <TaskCard Month='Fev' Day={17} Title='Dentista' BoxColor='#FFC700'></TaskCard>
+          </HStack>
+          <HStack>
+            <TaskCard Month='Fev' Day={17} Title='Dentista' BoxColor='#FFC700'></TaskCard>
+            <TaskCard Month='Fev' Day={17} Title='Dentista' BoxColor='#FFC700'></TaskCard>
+            <TaskCard Month='Fev' Day={17} Title='Dentista' BoxColor='#FFC700'></TaskCard>
+          </HStack>
+          <HStack>
+            <TaskCard Month='Fev' Day={17} Title='Dentista' BoxColor='#FFC700'></TaskCard>
+            <TaskCard Month='Fev' Day={17} Title='Dentista' BoxColor='#FFC700'></TaskCard>
+            <TaskCard Month='Fev' Day={17} Title='Dentista' BoxColor='#FFC700'></TaskCard>
+          </HStack>
+          <HStack>
+            <TaskCard Month='Fev' Day={17} Title='Dentista' BoxColor='#FFC700'></TaskCard>
+            <TaskCard Month='Fev' Day={17} Title='Dentista' BoxColor='#FFC700'></TaskCard>
+            <TaskCard Month='Fev' Day={17} Title='Dentista' BoxColor='#FFC700'></TaskCard>
+          </HStack>
+          <HStack>
+            <TaskCard Month='Fev' Day={17} Title='Dentista' BoxColor='#FFC700'></TaskCard>
+            <TaskCard Month='Fev' Day={17} Title='Dentista' BoxColor='#FFC700'></TaskCard>
+            <TaskCard Month='Fev' Day={17} Title='Dentista' BoxColor='#FFC700'></TaskCard>
+          </HStack>
+          <HStack>
+            <TaskCard Month='Fev' Day={17} Title='Dentista' BoxColor='#FFC700'></TaskCard>
+            <TaskCard Month='Fev' Day={17} Title='Dentista' BoxColor='#FFC700'></TaskCard>
+            <TaskCard Month='Fev' Day={17} Title='Dentista' BoxColor='#FFC700'></TaskCard>
+          </HStack>
+          
         </VStack>
+        </ScrollView>
       </VStack>
     </ImageBackground>
   );
